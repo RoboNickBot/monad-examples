00c356b76157862ad85418dde19125111117e2c0
RoboNickBot <nicklewchenko92@gmail.com>
2015-04-16 20:15:49 -0500

    started documentation

--- Crushes.hs
+++ Crushes.hs
@@ -1,35 +1,47 @@
+-- | Module    : Education.Monad.Examples.Crushes
+-- Copyright   : 2015 RoboNickBot
+-- License     : GPL-3
+-- Maintainer  : nicklewchenko92@gmail.com
+-- Stability   : experimental
+-- Portability : portable
+--
+-- Using the Maybe Monad: Periodic High-school of the Elements
+
 module Crushes where
 
 import Control.Monad
 import Control.Applicative
 import qualified Data.Map as M
-import qualified Data.List as L
 
+
+-- | A 'CrushDB' is a map of all /known/ crushes in the school, where
+-- the keys are the names of elements that are crushing, and the
+-- values are the names of elements that are being crushed upon.
 type CrushDB = M.Map String String
 
 sampleCrushDB :: CrushDB 
-sampleCrushDB = M.fromList [ ("carbon"   ,  "aluminum")
-                           , ("iron"     ,  "helium"  )
-                           , ("helium"   ,  "iron"    )
-                           , ("argon"    ,  "radium"  )
-                           , ("radium"   ,  "carbon"  )
-                           , ("hydrogen" ,  "carbon"  )
-                           , ("lithium"  ,  "neon"    ) ]
+sampleCrushDB = M.fromList [ ("Carbon"   ,  "Aluminum")
+                           , ("Iron"     ,  "Helium"  )
+                           , ("Helium"   ,  "Iron"    )
+                           , ("Argon"    ,  "Radium"  )
+                           , ("Radium"   ,  "Carbon"  )
+                           , ("Hydrogen" ,  "Carbon"  )
+                           , ("Lithium"  ,  "Neon"    ) ]
 
 crush :: CrushDB -> String -> Maybe String
 crush db a = M.lookup a db
 
--- implemented with (>>=)
-opponent :: CrushDB -> String -> Maybe String
-opponent db a = crush db a >>= crush db
-
--- and implemented without (>>=)
+-- implemented without (>>=)
 opponent' :: CrushDB -> String -> Maybe String
 opponent' db a = case crush db a of
                    Just b -> case crush db b of
                                Just c -> Just c
                                _ -> Nothing
                    _ -> Nothing
+
+-- implemented with (>>=)
+opponent :: CrushDB -> String -> Maybe String
+opponent db a = crush db a >>= crush db
 
 isRequited :: CrushDB -> String -> Maybe Bool
 isRequited db a = fmap (== a) (opponent db a)
